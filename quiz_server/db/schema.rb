bd1b89b8db4a0341febc084dad063fe6e04ac0b0
松山勇輝 <matsuMac@MacBook-Pro.local>
2015-02-14 13:12:03 +0900

    create question type model

--- quiz_server/db/schema.rb
+++ quiz_server/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150214015951) do
+ActiveRecord::Schema.define(version: 20150214040752) do
 
   create_table "events", force: true do |t|
     t.integer  "admin_user_id",                 null: false
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 20150214015951) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "is_delete",     default: false, null: false
+  end
+
+  create_table "question_types", force: true do |t|
+    t.string   "name",       default: "", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "questions", force: true do |t|
