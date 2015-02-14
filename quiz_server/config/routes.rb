dc5a7d16cf7c4a3509d034f9f9f7fde621bbba5b
松山勇輝 <matsuMac@MacBook-Pro.local>
2015-02-15 00:47:45 +0900

    fix routes.rb

--- quiz_server/config/routes.rb
+++ quiz_server/config/routes.rb
@@ -10,7 +10,6 @@ Rails.application.routes.draw do
   root to: "admin#index"
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
