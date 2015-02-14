abc3f5e8422c047c35344b7791d2f68cac4d4546
松山勇輝 <matsuMac@MacBook-Pro.local>
2015-02-14 16:31:05 +0900

    add admin/index to routes

--- quiz_server/config/routes.rb
+++ quiz_server/config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+
+
   devise_for :admin_users
+  get 'admin/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  root to: "admin#index"
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
