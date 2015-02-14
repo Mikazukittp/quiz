dcc4699770622b1c2ce1e7c8157fce63ba2fcfdd
松山勇輝 <matsuMac@MacBook-Pro.local>
2015-02-14 16:58:18 +0900

    add access restriction

--- quiz_server/app/controllers/admin_controller.rb
+++ quiz_server/app/controllers/admin_controller.rb
@@ -1,5 +1,12 @@
 class AdminController < ApplicationController
 
+    # ユーザがログインしていないと"show"にアクセスできない
+    before_action :authenticate_admin_user!, except: :index
+
     def index
+    end
+
+    def show
+
     end
 end
