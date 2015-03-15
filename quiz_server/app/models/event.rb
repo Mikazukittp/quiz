d5aea5de22b8bc241e7d8d66670081ce95177228
松山勇輝 <matsuMac@MacBook-Pro.local>
2015-03-15 19:52:15 +0900

    add association between  models

--- quiz_server/app/models/event.rb
+++ quiz_server/app/models/event.rb
@@ -1,2 +1,7 @@
 class Event < ActiveRecord::Base
+    has_many :questions
+    has_many :answerers
+    has_many :payments
+    has_one :course
+    belongs_to :admin_user
 end
--- quiz_server/app/models/question.rb
+++ quiz_server/app/models/question.rb
@@ -1,2 +1,6 @@
 class Question < ActiveRecord::Base
+    has_many :choises
+    has_one :question_type
+    has_many :answerers
+    belongs_to :event
 end
