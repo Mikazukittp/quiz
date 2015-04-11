81d96419a6bad32b4ad643a9877f15c2ae173b23
matsumatsu20 <matsumatsu20@gmail.com>
2015-04-11 11:09:58 +0900

    add show method

--- quiz_server/app/controllers/answerers_controller.rb
+++ quiz_server/app/controllers/answerers_controller.rb
@@ -11,6 +11,15 @@ class AnswerersController < ApplicationController
     def update
     end
 
+    def show
+        if ck = cookies[:quiz_user_token]
+            user = Answerer.find_by(user_token: ck)
+            render :json => user
+        else
+            render_fault("解答者の取得に失敗しました")
+        end
+    end
+
     def get_question
         if ck = cookies[:quiz_user_token]
             user = Answerer.find_by(user_token: ck)
@@ -22,5 +31,9 @@ class AnswerersController < ApplicationController
 
     def set_user_token_cookie(user_token)
         cookies[:quiz_user_token] = { :value => user_token, :expires => 1.days.from_now}
+    end
+
+    def is_exist_answerer?
+
     end
 end
