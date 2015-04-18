f2da329e2a0a910c2c7f10f0677eec84af6ba5e1
haijima <k.haijima@gmail.com>
2015-04-18 19:36:21 +0900

    イベントと問題の追加/削除を実装

--- client/app/scripts/controllers/admin/event.js
+++ client/app/scripts/controllers/admin/event.js
@@ -8,7 +8,7 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('AdminEventCtrl', function ($scope, $stateParams, events, questions) {
+  .controller('AdminEventCtrl', function ($scope, $stateParams, events, questions, $modal) {
 
     $scope.id = $stateParams.id;
     events.get({id: $scope.id}, function(data){
@@ -19,7 +19,89 @@ angular.module('clientApp')
       $scope.questions = data;
     });
 
+    $scope.createQuestion = function() {
+      var modalInstance = $modal.open({
+        templateUrl: 'createQuestion.html',
+        controller: 'CreateQuestionModalCtrl'
+      });
+      modalInstance.result.then(function (result) {
+        console.log(result);
+        questions.save({
+          question: {
+            event_id: $scope.id,
+            sentence: result.sentence,
+            points: 50,
+            type_id: 1
+          },
+          choices: {
+            choice1: {
+              sentence: result.choices[0],
+              choice_number: 1,
+              correct_flag: result.answer==1,
+              answered_times: 0
+            },
+            choice2: {
+              sentence: result.choices[1],
+              choice_number: 2,
+              correct_flag: result.answer==2,
+              answered_times: 0
+            },
+            choice3: {
+              sentence: result.choices[2],
+              choice_number: 3,
+              correct_flag: result.answer==3,
+              answered_times: 0
+            },
+            choice4: {
+              sentence: result.choices[3],
+              choice_number: 4,
+              correct_flag: result.answer==4,
+              answered_times: 0
+            }
+          }
+        }, function(data){
+          console.log(data);
+          // $scope.eventsに追加
+          $scope.questions.push(data.question);
+        });
+      });
+    };
+
     $scope.deleteQuestion = function(id) {
-      questions.delete(id);
-    }
+      var modalInstance = $modal.open({
+        templateUrl: 'deleteQuestion.html',
+        controller: 'DeleteModalCtrl'
+      });
+      modalInstance.result.then(function () {
+        questions.delete({id:id}, function(){
+            $scope.questions.forEach(function(q, i){
+              if(q.id==id) $scope.questions.splice(i,1);
+            });
+        });
+      });
+    };
+  })
+  .controller('CreateQuestionModalCtrl', function ($scope, $modalInstance) {
+    $scope.answer = 1;
+    $scope.ok = function () {
+      var result = {
+        sentence: $scope.sentence,
+        choices: [$scope.c1, $scope.c2, $scope.c3, $scope.c4],
+        answer: $scope.answer
+      }
+      $modalInstance.close(result);
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+  })
+  .controller('DeleteModalCtrl', function ($scope, $modalInstance) {
+    $scope.ok = function () {
+      $modalInstance.close();
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
   });
--- client/app/scripts/controllers/admin/user.js
+++ client/app/scripts/controllers/admin/user.js
@@ -8,17 +8,70 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('AdminUserCtrl', function ($scope, events, Auth) {
+  .controller('AdminUserCtrl', function ($scope, events, Auth, $modal) {
     // var loginId = 1;
     var loginId = Auth.getCurrentUser().id;
     events.findByUserId({user_id: loginId}, function(data){
       console.log(data);
       $scope.events = data;
     });
+
     $scope.createEvent = function() {
-      events.create({}, function(data){
-        console.log(data);
-        $scope.$apply();
+      var modalInstance = $modal.open({
+        templateUrl: 'createEvent.html',
+        controller: 'CreateEventModalCtrl'
       });
+      modalInstance.result.then(function (result) {
+        events.save({
+          event: {
+            name: result.name,
+            description: result.description,
+            event_date: '2015/5/1',
+            limit_date: '2015/5/8',
+            time_limit: 60,
+            course_id: 1
+          }
+        }, function(data){
+          console.log(data);
+          $scope.events.push(data.event);
+        });
+      });
+    };
+
+    $scope.deleteEvent = function(id) {
+      var modalInstance = $modal.open({
+        templateUrl: 'deleteEvent.html',
+        controller: 'DeleteModalCtrl'
+      });
+      modalInstance.result.then(function () {
+        events.delete({id:id}, function(){
+            $scope.events.forEach(function(e, i){
+              if(e.id==id) $scope.events.splice(i,1);
+            });
+        });
+      });
+    };
+
+  })
+  .controller('CreateEventModalCtrl', function ($scope, $modalInstance) {
+    $scope.ok = function () {
+      var result = {
+        name: $scope.name,
+        description: $scope.description
+      }
+      $modalInstance.close(result);
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+  })
+  .controller('DeletModalCtrl', function ($scope, $modalInstance) {
+    $scope.ok = function () {
+      $modalInstance.close();
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
     };
   });
