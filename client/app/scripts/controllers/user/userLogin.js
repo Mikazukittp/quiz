65e8238f6c9941b17d2caf2cc012aa092edff9c6
haijima <k.haijima@gmail.com>
2015-05-10 17:28:39 +0900

    エラーメッセージを表示するように修正

--- client/app/scripts/controllers/user/userLogin.js
+++ client/app/scripts/controllers/user/userLogin.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('clientApp')
-.controller('UserLoginCtrl', function ($scope,Auth, $location,$stateParams) {
+.controller('UserLoginCtrl', function ($rootScope, $scope, $location, $stateParams, Auth) {
   $scope.user = {};
   $scope.errors = {};
-  $scope.id = $stateParams.eventId; 
+  $scope.id = $stateParams.eventId;
 
   $scope.login = function(form) {
     $scope.submitted = true;
@@ -14,13 +14,14 @@ angular.module('clientApp')
         id:$scope.user.id,
       })
       .then( function() {
-          console.log('login success');
-          // Logged in, redirect to home
-          $location.path('/user/question/0');
-        })
+        console.log('login success');
+        // Logged in, redirect to home
+        $location.path('/user/question/0');
+      })
       .catch( function(err) {
-          console.log(err);
+        console.log(err);
         $scope.errors.other = err;
+        $rootScope.$broadcast('error-message', {message: err.info});
       });
     }
   };
